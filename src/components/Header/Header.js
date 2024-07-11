85685b5f244a1d5d75a6fd372654163e00ac9cf7
sari-rahul <sarisivadas93@gmail.com>
2024-07-11 10:40:00 +0200

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delicous array of 
+               dishes crafted with the finest ingredients and culinary expertise.
+               Our mission is to satisfy your cravings and elevate your dinig experince.
+               one delicoius meal ata time.
+            </p>
+            <button>View Menu</button>
+        </div>  
+    </div>
+  )
+}
+
+export default Header
